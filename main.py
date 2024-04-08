676f2277d4dca91352db4fc2bdc75fc1c236f591
pyrtaAV <pyrta90@gmail.com>
2024-04-09 01:01:07 +0400

    python task for it-mentor from Alexey Pyrta

---
+++ main.py
@@ -0,0 +1,39 @@
+def main(calc_data: str):
+    calc_list = calc_data.split(' ')
+    operators = ['+', '-', '*', '/']
+
+    if len(calc_list) < 3 or len(calc_list) > 3:
+        return Exception('throws Exception 1')
+
+    try:
+        left_operand = int(calc_list[0])
+        right_operand = int(calc_list[2])
+    except:
+        return Exception('throws Exception 2')
+
+    if left_operand > 10 or right_operand > 10:
+        return Exception('throws Exception 3')
+
+    if calc_list[1] in operators:
+        return calculation(left_operand, right_operand, calc_list[1])
+    else:
+        return Exception('throws Exception 4')
+
+
+def calculation(left_operand: int, right_operand: int, operator: str):
+    if operator == '+':
+        return left_operand + right_operand
+    elif operator == '-':
+        return left_operand - right_operand
+    elif operator == '*':
+        return left_operand * right_operand
+    elif operator == '/':
+        try:
+            return left_operand // right_operand
+        except:
+            return Exception('throws Exception 5')
+
+
+calc_input = input()
+p = main(calc_input)
+print(p)
